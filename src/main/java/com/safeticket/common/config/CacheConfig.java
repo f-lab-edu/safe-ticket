fc3a31ba28cc2a3d7473e8ed68e37cce8b170590
gominnam <kosok03@naver.com>
2025-02-21 15:21:02 +0900

    feat: EHcache 설정 파일 추가

--- src/main/java/com/safeticket/common/config/CacheConfig.java
+++ src/main/java/com/safeticket/common/config/CacheConfig.java
@@ -1,16 +1,27 @@
 package com.safeticket.common.config;
 
+import org.ehcache.jsr107.EhcacheCachingProvider;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+import java.net.URISyntaxException;
+import java.util.Objects;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
     @Bean
-    public ConcurrentMapCacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("availableTickets");
+    public JCacheCacheManager cacheManager() throws URISyntaxException {
+        CachingProvider provider = Caching.getCachingProvider(EhcacheCachingProvider.class.getName());
+        CacheManager cacheManager = provider.getCacheManager(
+                Objects.requireNonNull(getClass().getResource("/ehcache.xml")).toURI(),
+                provider.getDefaultClassLoader());
+        return new JCacheCacheManager(cacheManager);
     }
 }
