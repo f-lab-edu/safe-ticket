781bfdd94dd4096d4fc4a9504173badf11ec8143
gominnam <kosok03@naver.com>
2025-02-28 19:12:19 +0900

    fix: java build test 오류 수정, 모니터링 빌드 오류 script 수정

--- src/test/java/com/safeticket/SafeTicketApplicationTests.java
+++ src/test/java/com/safeticket/SafeTicketApplicationTests.java
@@ -2,8 +2,10 @@ package com.safeticket;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SafeTicketApplicationTests {
 
     @Test
