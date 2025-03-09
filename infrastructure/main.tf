c732b43fa43db90f9373b8a18fb98ac071d03bf6
gominnam <kosok03@naver.com>
2025-03-09 14:39:32 +0900

    feat: 테라폼 main.tf 인바운드 규칙 추가

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,10 +15,59 @@ variable "instances" {
   ]
 }
 
+# 키페어 설정
+resource "aws_key_pair" "ticket_server_key" {
+   key_name   = "ticket-server-key"
+   public_key = file("./ticket-server.pub")
+}
+
+# 보안 그룹
+resource "aws_security_group" "allow_prometheus_grafana_spring" {
+  name        = "allow-prometheus-grafana-spring"
+  description = "Allow access to Prometheus (9090) and Grafana (3000) and ticket-app(8080), SSH (22)"
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "servers" {
   count         = length(var.instances)
   ami           = var.instances[count.index].ami
   instance_type = var.instances[count.index].instance_type
+  key_name      = aws_key_pair.ticket_server_key.key_name
+  security_groups = [aws_security_group.allow_prometheus_grafana_spring.name]
 
   tags = {
     Name = var.instances[count.index].name
@@ -28,4 +77,3 @@ resource "aws_instance" "servers" {
 output "ec2_instances" {
   value = aws_instance.servers[*].public_ip
 }
-
