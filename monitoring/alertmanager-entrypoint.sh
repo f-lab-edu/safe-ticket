919656e990e82c7a1dd3ce3831acae838e1f3761
gominnam <kosok03@naver.com>
2025-03-11 18:20:56 +0900

    test: 모니터링 연동 테스트

--- monitoring/alertmanager-entrypoint.sh
+++ monitoring/alertmanager-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# 환경 변수를 템플릿 파일에 반영
-envsubst < /etc/alertmanager/alertmanager.yml > /etc/alertmanager/alertmanager.yml
+# Apply environment variables to the template file
+envsubst < /etc/alertmanager/alertmanager.yml.template > /etc/alertmanager/alertmanager.yml
 
-# Alertmanager 실행
+# Run Alertmanager
 exec /bin/alertmanager --config.file=/etc/alertmanager/alertmanager.yml
