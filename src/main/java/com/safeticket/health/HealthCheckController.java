ad83e056de0bfb87b972c8c4fede6f0e39fc09e2
gominnam <kosok03@naver.com>
2025-03-25 23:20:24 +0900

    fix: aws alb health 체크 api url 수정

--- src/main/java/com/safeticket/health/HealthCheckController.java
+++ src/main/java/com/safeticket/health/HealthCheckController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/healthy")
+    @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("server is healthy");
     }
