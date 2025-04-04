567efe0831a3ee0717f95190a313726d8057941b
gominnam <kosok03@naver.com>
2025-04-04 20:14:10 +0900

    fix: Order와 PaymentMessage 매핑 오류 수정

--- order/src/main/java/com/safeticket/order/mapper/OrderMapper.java
+++ order/src/main/java/com/safeticket/order/mapper/OrderMapper.java
@@ -4,15 +4,31 @@ import com.safeticket.order.dto.OrderDTO;
 import com.safeticket.order.entity.Order;
 import com.ticket.common.dto.PaymentMessage;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface OrderMapper {
     OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
 
+    @Mappings({
+            @Mapping(target = "status", ignore = true)
+    })
     OrderDTO toOrderDTO(PaymentMessage paymentMessage);
+    @Mappings({
+            @Mapping(target = "status", ignore = true)
+    })
     PaymentMessage toPaymentMessage(OrderDTO orderDTO);
 
-    PaymentMessage toPaymentMessage(Order order);
+    @Mappings({
+            @Mapping(source = "orderId", target = "id"),
+            @Mapping(target = "status", ignore = true)
+    })
     Order toOrder(PaymentMessage paymentMessage);
+    @Mappings({
+            @Mapping(source = "id", target = "orderId"),
+            @Mapping(target = "status", ignore = true)
+    })
+    PaymentMessage toPaymentMessage(Order order);
 }
